897587813f3fd607f31a203a2256ff6f3f26f752
manjusa <thizismanju@gmail.com>
2020-04-17 11:15:27 +1000

    Update email.php

--- misc/email.php
+++ misc/email.php
@@ -6,21 +6,26 @@ $name ='';
 $email ='';
 $phoneNumber ='';
 $comment='';
+$bodystart='My comment is: ';
+$bodyemail=' My email id is: ';
+$bodyph=' My mobile is: ';
 $name = preg_replace("/[^a-zA-Z]/", "", $_REQUEST['name']);
 $comment = preg_replace("/[^a-zA-Z]/", "", $_REQUEST['comment']);
 $email = preg_replace("/[^a-zA-Z]/", "", $_REQUEST['email']);
 $phoneNumber = preg_replace("/[^a-zA-Z]/", "", $_REQUEST['phone']);
-$santiziedEmail = filter_input(INPUT_GET,  
-            'email', FILTER_SANITIZE_SPECIAL_CHARS); 
-$santiziedComment = filter_input(INPUT_GET,  
-            'comment', FILTER_SANITIZE_SPECIAL_CHARS); 
+ 
 echo '<h4>Thanks for your message. I will get back to you soon</h4><br>
         <a href="/index.html">Go Back to Home Page</a><br>
       ';
-echo $santiziedEmail;
 $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
-$txt = "name:".$name.",comment:". $santiziedComment.",email:".$santiziedEmail.",phoneNumber:".$phoneNumber."---------";
-fwrite($myfile,"\n". $txt);
+/*$txt = "name: ".$name." ,comment: ". $comment.",email:".$email.",phoneNumber:".$phoneNumber."---------";
+fwrite($myfile,"\n". $txt);*/
 
+
+$to = "vnmshenoy@gmail.com";
+$subject = "new enquiry";
+$body =  "Hello My name is: ".$name.", \n and my comment is: ".$comment." . \n Please reach out to me at ".$phoneNumber." OR \n  email".$email;
+$headers = "From: ipage" . "\r\n" .
+mail($to,$subject,$body,$headers);
 fclose($myfile);
 ?>
